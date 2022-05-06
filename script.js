1212df771b8a79066434cb8a6f28c764676c2942
puun555 <105053170+puun555@users.noreply.github.com>
2022-05-06 20:10:48 +0700

    multi-web

---
+++ script.js
@@ -0,0 +1,50 @@
+let picp1 = document.getElementById('picp1');
+let picp2 = document.getElementById('picp2');
+let picp4 = document.getElementById('picp4');
+let oneday = document.getElementById('day');
+
+    window.addEventListener('scroll', function(){
+      let value = window.scrollY;
+      picp1.style.left = value * -0.25 + 'px';
+      picp1.style.top = value * 0.25 + 'px';
+      picp2.style.top = value * 0.35 + 'px';
+      picp4.style.top = value * 0.33 - 900 + 'px';
+    })
+
+
+//----------------------------------------------------------------- page8scipt
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      // dots[slideIndex-1].className += " active";
+    }
+// ----------------------------------------------------------------------
+  function changepic1() {
+    pic11.style.setProperty("background", "url('https://media.discordapp.net/attachments/956540505792073778/971797739723686019/unknown.png') no-repeat")
+    pic11.style.setProperty("background-size", "contain")
+  }
+    function changepic2() {
+      pic11.style.setProperty("background", "url('https://media.discordapp.net/attachments/956540505792073778/971797764444930158/unknown.png') no-repeat")
+      pic11.style.setProperty("background-size", "contain")
+    }
